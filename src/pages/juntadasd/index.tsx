4c86307d896f032ea84c00d97c362cd02b68a6a3
unknown <franciscogerardi@gmail.com>
2023-06-02 00:27:40 +0300

    add rerrouting to sdmeeting

---
+++ src/pages/juntadasd/index.tsx
@@ -0,0 +1,14 @@
+import {  useEffect } from "react";
+
+const Redirect = () => {
+
+    useEffect(() => {
+        window.location.href = "https://sites.google.com/laia.ar/juntada-sd";
+    }, []);
+
+
+    return <></>;
+};
+
+export default Redirect;
+
